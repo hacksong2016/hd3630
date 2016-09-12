96fb97341d17ce3293c547e83f535eaa0eb51a2d
sunhannan <cto@fami2u.com>
2016-09-12 18:56:53 +0800

    fix some issue

--- server/wechat/config.js
+++ server/wechat/config.js
@@ -1,22 +1,8 @@
 wechatConfig = {
-	// appid:"wx6487235267a787f7",
-	// secret:"3ba5e0f80eef9566526f50eba48b625c",
-	// mchid:"1232233802",
-	// partnerKey:"",
 
-
-	// appid:"wxe8f39b5b477a688c",
-	// secret:"b91814b6d38c6550e94c617dcf6e1f29",
-	// mchid:"1363053902",
-	// partnerKey:"rtohjwipu4hjt3qjtoq3jt3qkt3ktjoq",
-
-	appid:"wxe8f39b5b477a688c",
- 	secret:"b91814b6d38c6550e94c617dcf6e1f29",
- 	mchid:"1251959901",
- 	partnerKey:"bqigb4jrhblkjewlkvjanadbnbgiblnb",
+	appid:"",
+ 	secret:"",
+ 	mchid:"",
+ 	partnerKey:"",
  	
- 	// appid:"wx24b870b73f9c45ee",
- 	// secret:"5028e81db0a2d03ae46cd411511578d3",
- 	// mchid:"1363053902",
- 	// partnerKey:"rtohjwipu4hjt3qjtoq3jt3qkt3ktjoq",
 }
